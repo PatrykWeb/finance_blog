bbad91ea25f58e7706bbc686177e098f5f4975f4
devWiseguy <hillj1089@gmail.com>
2021-02-04 23:02:56 +0100

    adding a simple controller that adds data to the database using a form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,21 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @counter = Category.count + 1
+  end
+
+  def create
+    @category = Category.new(category_parametr)
+    if @category.save
+      redirect_to(:action => 'index')
+    else
+      @counter = Category.count + 1
+      render('new')
+    end
+  end
+
+  def category_parametr
+    params.require(:category).permit(:name, :position, :visible)
   end
 
   def edit
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ApplicationRecord
   has_many :pages
 
-  scope :visible, -> lambda{where(:visible => true)}
-  scope :unvisible, -> lambda{where(:visible => false)}
-  scope :sort_asc, -> lambda{order("categories.position ASC")}
-  scope :latest_category, -> lambda{order("categories.created_at DESC")}
+  scope :visible, -> {where(:visible => true)}
+  scope :unvisible, -> {where(:visible => false)}
+  scope :sort_asc, -> {order("categories.position ASC")}
+  scope :latest_category, -> {order("categories.created_at DESC")}
 
 end
